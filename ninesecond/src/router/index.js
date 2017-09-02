e3ab6f6f307c02661fa11db76bd48b57f8fd4c62
sanSanChen <15138095613@163.com>
2017-09-02 14:09:56 +0800

    配置异步加载

--- ninesecond/src/router/index.js
+++ ninesecond/src/router/index.js
@@ -1,14 +1,37 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Public from '../components/Public.vue';
-import Homepage from '../components/Homepage.vue';
-import Product from '../components/Product.vue';
-import Solution from '../components/Solution.vue';
-import Classiccase from "../components/Classiccase.vue";
-import About from '../components/About.vue';
-import Joinus from '../components/Joinus.vue';
-import Contactus from '../components/Contactus.vue';
-
+// import Public from '../components/Public.vue';
+// import Homepage from '../components/Homepage.vue';
+// import Product from '../components/Product.vue';
+// import Solution from '../components/Solution.vue';
+// import Classiccase from "../components/Classiccase.vue";
+// import About from '../components/About.vue';
+// import Joinus from '../components/Joinus.vue';
+// import Contactus from '../components/Contactus.vue';
+require.ensure([],(require)=>{
+  const Public = ()=>require('../components/Public.vue');
+},'Public');
+require.ensure([],(require)=>{
+  const Homepage = ()=>require('../components/Homepage.vue');
+},'Homepage');
+require.ensure([],(require)=>{
+  const Product = ()=>require('../components/Product.vue');
+},'Product');
+require.ensure([],(require)=>{
+  const Solution = ()=>require('../components/Solution.vue');
+},'Solution');
+require.ensure([],(require)=>{
+  const Classiccase = ()=>require('../components/Classiccase.vue');
+},'Classiccase');
+require.ensure([],(require)=>{
+  const About = ()=>require('../components/About.vue');
+},'About');
+require.ensure([],(require)=>{
+  const Joinus = ()=>require('../components/Joinus.vue');
+},'Joinus');
+require.ensure([],(require)=>{
+  const Contactus = ()=>require('../components/Contactus.vue');
+},'Contactus');
 Vue.use(Router);
 
 export default new Router({
