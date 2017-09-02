fd316743b17fd77e95037cc46436210f2dc388ce
sanSanChen <15138095613@163.com>
2017-09-02 14:48:03 +0800

    “vue-router+webpack(require.ensure)异步加载组件”

--- ninesecond/src/router/index.js
+++ ninesecond/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { Indicator } from 'mint-ui';
 // import Public from '../components/Public.vue';
 // import Homepage from '../components/Homepage.vue';
 // import Product from '../components/Product.vue';
@@ -10,59 +9,43 @@ import { Indicator } from 'mint-ui';
 // import Joinus from '../components/Joinus.vue';
 // import Contactus from '../components/Contactus.vue';
 const Public=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Public.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Homepage=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Homepage.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Product=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Product.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Solution=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Solution.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Classiccase=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Classiccase.vue'));
-		Indicator.close()
 	},'Public');
 }
 const About=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/About.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Joinus=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Joinus.vue'));
-		Indicator.close()
 	},'Public');
 }
 const Contactus=resolve=>{
-	Indicator.open();
 	require.ensure([],()=>{
 		resolve(require('../components/Contactus.vue'));
-		Indicator.close()
 	},'Public');
 }
 Vue.use(Router);
